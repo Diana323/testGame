e9ea5df64118080ef77e2a1da9ea03e2bea2a36d
Діана Пахалюк <110489496+Diana323@users.noreply.github.com>
2024-01-12 01:35:52 +0200

    создан
    не отображается главный герой(квадрат)

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
 import pygame
-pygame.init()
-HEIGHT = 800
+from pygame.constants import QUIT
 
+pygame.init()
+
+FPS = pygame.time.Clock()
+
+HEIGHT = 800
 WIDTH = 1200
 
+COLOR_WHITE = (255, 255, 255)
+COLOR_BLACK = (0, 0, 0)
+
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
-surf = pygame.Surface((150, 150))
+
+player_size = (20, 20)
+player = pygame.Surface(player_size)
+player.fill(COLOR_WHITE)
+player_rect = player.get_rect()
+player_speed = [1, 1]
+
+playing = True
+
+while playing:
+   FPS.tick(120)
+
+   for event in pygame.event.get():
+      if event.type == QUIT:
+         playing = False
+
+main_display.fill(COLOR_BLACK)
+
+if player_rect >=HEIGHT:
+   player_rect = [1, -1]
+
+main_display.blit(player, player_rect)
+
+player_rect = player_rect.move(player_speed)
+
+pygame.display.flip()
+     
