5bcbdaeff43ea36c06b869bc342aa889ec929e6c
Діана Пахалюк <110489496+Diana323@users.noreply.github.com>
2024-01-12 23:22:19 +0200

    рухи на кнопки
    герой рухається при натискання на кнопки

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
+
 import pygame
-from pygame.constants import QUIT
+from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 
 pygame.init()
 
@@ -17,7 +19,13 @@ player_size = (20, 20)
 player = pygame.Surface(player_size)
 player.fill(COLOR_WHITE)
 player_rect = player.get_rect()
-player_speed = [1, 1]
+player_move_down = [0, 1]
+player_move_up = [0, -1]
+player_move_right = [1, 0]
+player_move_left = [-1, 0]
+# player_speed = [0, 5]
+# player_rect.y += player_speed[1] * player_move_up[1]
+# player_rect.x += player_speed[0] * player_move_left[0]
 
 playing = True
 
@@ -27,18 +35,18 @@ while playing:
         if event.type == QUIT:
             playing = False
     main_display.fill(COLOR_BLACK)
-    player_rect = player_rect.move(player_speed)
+
+    keys = pygame.key.get_pressed()
+
+    if keys[K_DOWN] and player_rect.bottom < HEIGHT:
+        player_rect = player_rect.move(player_move_down)
+    if keys[K_UP] and player_rect.top > 0:
+        player_rect = player_rect.move(player_move_up)
+    if keys[K_RIGHT] and player_rect.right < WIDTH:
+        player_rect = player_rect.move(player_move_right)
+    if keys[K_LEFT] and player_rect.left > 0:
+        player_rect = player_rect.move(player_move_left)
 
     main_display.blit(player, player_rect)
-
-    if player_rect.bottom >= HEIGHT or player_rect.top <= 0:
-        player_speed[1] = -player_speed[1]
-
-    if player_rect.right >= WIDTH or player_rect.left <= 0:
-        player_speed[0] = -player_speed[0]
-
-    # player_rect = player_rect.move(player_speed)
-
-    # main_display.blit(player, player_rect)
 
     pygame.display.flip()
